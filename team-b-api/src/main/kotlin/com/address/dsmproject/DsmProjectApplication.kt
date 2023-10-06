eefe794c60dc1d42c5af62870cb161bccf578d0c
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-06 19:57:09 +0900

    setting :: api 모듈 세팅

---
+++ team-b-api/src/main/kotlin/com/address/dsmproject/DsmProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.address.dsmproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DsmProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<DsmProjectApplication>(*args)
+}
