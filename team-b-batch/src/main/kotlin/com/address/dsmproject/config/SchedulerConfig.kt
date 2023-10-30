3616e9c8b46730818e103d32e39252074c31235f
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-30 22:28:58 +0900

    init :: SchedulerConfig 추가

---
+++ team-b-batch/src/main/kotlin/com/address/dsmproject/config/SchedulerConfig.kt
@@ -0,0 +1,8 @@
+package com.address.dsmproject.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@EnableScheduling
+@Configuration
+class SchedulerConfig
