7d2a8bee978703f2371ecfa56959d7263b92af4b
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-30 22:27:08 +0900

    chore :: common 패키지에서 Mysql 의존성 제거

--- team-b-common/build.gradle.kts
+++ team-b-common/build.gradle.kts
@@ -1,16 +1,13 @@
 plugins {
-    id("org.springframework.boot") version "3.1.4"
-    kotlin("plugin.spring") version "1.6.21"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("plugin.jpa") version "1.6.21"
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 dependencies {
     // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    // MySQL
-    runtimeOnly("mysql:mysql-connector-java")
 }
 
 tasks.getByName<Jar>("bootJar") {
