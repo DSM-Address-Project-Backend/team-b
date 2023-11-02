7ea5c9b61693b7fe05e1c8477569df8aa52ded4d
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-02 20:01:03 +0900

    chore :: streetNumber 컬럼 제거

--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadAddress/RoadAddressEntity.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadAddress/RoadAddressEntity.kt
@@ -1,21 +1,19 @@
 package com.address.dsmproject.domain.roadAddress
 
 import com.address.dsmproject.global.entity.BaseUUIDEntity
-import jakarta.persistence.*
-import java.util.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "tbl_road_address")
-class RoadAddressEntity (
+class RoadAddressEntity(
 
     @Column(columnDefinition = "INT", nullable = false)
     val mainBuildingNumber: Int,
 
     @Column(columnDefinition = "INT", nullable = false)
     val subBuildingNumber: Int,
-
-    @Column(columnDefinition = "VARCHAR(80)", nullable = false)
-    val streetNumber: String,
 
     @Column(columnDefinition = "INT", nullable = false)
     val postalCode: Int,
