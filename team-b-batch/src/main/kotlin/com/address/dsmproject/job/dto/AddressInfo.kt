251a26cbbcd92e851655995339c9418bfb6ee822
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-03 08:53:55 +0900

    refactor :: return 명시 제거

--- team-b-batch/src/main/kotlin/com/address/dsmproject/job/dto/AddressInfo.kt
+++ team-b-batch/src/main/kotlin/com/address/dsmproject/job/dto/AddressInfo.kt
@@ -4,7 +4,6 @@ import com.address.dsmproject.domain.parcelNumber.ParcelNumberEntity
 import com.address.dsmproject.domain.roadAddress.RoadAddressEntity
 import com.address.dsmproject.domain.roadNumber.RoadNumberEntity
 import com.address.dsmproject.domain.roadNumber.RoadNumberId
-import com.address.dsmproject.job.AddressInfo
 import java.util.UUID
 
 data class AddressInfo(
@@ -24,39 +23,35 @@ data class AddressInfo(
     val subAddressNumber: Int,
 )
 
-fun AddressInfo.toParcelNumberEntity(): ParcelNumberEntity {
-    return ParcelNumberEntity(
-        id = UUID.randomUUID(),
-        mainAddressNumber = this.mainAddressNumber,
-        subAddressNumber = this.subAddressNumber
-    )
-}
+fun AddressInfo.toParcelNumberEntity() = ParcelNumberEntity(
+    id = UUID.randomUUID(),
+    mainAddressNumber = this.mainAddressNumber,
+    subAddressNumber = this.subAddressNumber
+)
 
-fun AddressInfo.toRoadNumberEntity(parcelNumber: ParcelNumberEntity, roadAddressEntity: RoadAddressEntity): RoadNumberEntity {
-    return RoadNumberEntity(
-        roadNumberId = RoadNumberId(
-            parcelNumberId = parcelNumber.id,
-            roadAddressId = roadAddressEntity.id
-        ),
-        cityProvinceName = this.cityProvinceName,
-        countyDistricts = this.countyDistricts,
-        eupMyeonDong = this.eupMyeonDong,
-        beobJeongLi = this.beobJeongLi,
-        cityProvinceNameEng = this.cityProvinceNameEng,
-        countyDistrictsEng = this.countyDistrictsEng,
-        eupMyeonDongEng = this.eupMyeonDongEng,
-        beobJeongLiEng = this.beobJeongLiEng,
-        parcelNumberEntity = parcelNumber,
-        roadAddressEntity = roadAddressEntity
-    )
-}
+fun AddressInfo.toRoadNumberEntity(
+    parcelNumber: ParcelNumberEntity,
+    roadAddressEntity: RoadAddressEntity,
+) = RoadNumberEntity(
+    roadNumberId = RoadNumberId(
+        parcelNumberId = parcelNumber.id,
+        roadAddressId = roadAddressEntity.id
+    ),
+    cityProvinceName = this.cityProvinceName,
+    countyDistricts = this.countyDistricts,
+    eupMyeonDong = this.eupMyeonDong,
+    beobJeongLi = this.beobJeongLi,
+    cityProvinceNameEng = this.cityProvinceNameEng,
+    countyDistrictsEng = this.countyDistrictsEng,
+    eupMyeonDongEng = this.eupMyeonDongEng,
+    beobJeongLiEng = this.beobJeongLiEng,
+    parcelNumberEntity = parcelNumber,
+    roadAddressEntity = roadAddressEntity
+)
 
-fun AddressInfo.toRoadAddressEntity(): RoadAddressEntity {
-    return RoadAddressEntity(
-        id = UUID.randomUUID(),
-        mainBuildingNumber = this.mainAddressNumber,
-        subBuildingNumber = this.subAddressNumber,
-        postalCode = this.postalCode,
-        buildingName = this.buildingName
-    )
-}
+fun AddressInfo.toRoadAddressEntity() = RoadAddressEntity(
+    mainBuildingNumber = this.mainAddressNumber,
+    subBuildingNumber = this.subAddressNumber,
+    postalCode = this.postalCode,
+    buildingName = this.buildingName
+)
