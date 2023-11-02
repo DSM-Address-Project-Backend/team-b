de75bcaf0fb41a18f09ce2eb4aea8b63c70efdc2
Kangmin <ehfehf0629@gmail.com>
2023-11-03 08:38:19 +0900

    feat: SaveJobConfiguration

--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadAddress/RoadAddressEntity.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadAddress/RoadAddressEntity.kt
@@ -4,10 +4,13 @@ import com.address.dsmproject.global.entity.BaseUUIDEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import java.util.UUID
 
 @Entity
 @Table(name = "tbl_road_address")
 class RoadAddressEntity(
+
+    id: UUID,
 
     @Column(columnDefinition = "INT", nullable = false)
     val mainBuildingNumber: Int,
@@ -20,4 +23,4 @@ class RoadAddressEntity(
 
     @Column(columnDefinition = "VARCHAR(40)", nullable = false)
     val buildingName: String,
-) : BaseUUIDEntity()
+) : BaseUUIDEntity(id)
--- team-b-common/src/main/kotlin/com/address/dsmproject/global/entity/BaseUUIDEntity.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/global/entity/BaseUUIDEntity.kt
@@ -8,11 +8,11 @@ import org.hibernate.annotations.GenericGenerator
 import java.util.UUID
 
 @MappedSuperclass
-abstract class BaseUUIDEntity {
+abstract class BaseUUIDEntity(
 
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     val id: UUID = UUID.randomUUID()
-}
+)
