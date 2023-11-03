2dcf410f9ec098b43e4a62818c2659508121fa09
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-03 15:27:36 +0900

    chore :: main 메소드에 @EnableScheduling 추가

--- team-b-batch/src/main/kotlin/com/address/dsmproject/config/SchedulerConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.address.dsmproject.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
-
-@EnableScheduling
-@Configuration
-class SchedulerConfig
