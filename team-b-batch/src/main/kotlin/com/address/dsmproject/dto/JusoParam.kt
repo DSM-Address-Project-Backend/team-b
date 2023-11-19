b78c353a283e30cd97ba608d5509c263302bcc1e
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-19 16:38:50 +0900

    chore :: 클래스명 변경

--- team-b-batch/src/main/kotlin/com/address/dsmproject/dto/JusoParam.kt
+++ team-b-batch/src/main/kotlin/com/address/dsmproject/dto/JusoParam.kt
@@ -1,9 +1,16 @@
 package com.address.dsmproject.dto
 
-data class AddressInfoParam(
+data class JusoParam(
+    val language: String,
     val year: String,
     val month: String,
     val yyMMZip: String,
     val fileName: String,
     val realFileName: String,
+)
+
+data class CommonJusoParam(
+    val yyyy: String,
+    val mm: String,
+    val yyyyMM: String,
 )
