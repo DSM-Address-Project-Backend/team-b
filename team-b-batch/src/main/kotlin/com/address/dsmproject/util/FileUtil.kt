bda301a5ea47d74b01927730699b7cbedc22f3b9
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-11-19 16:36:34 +0900

    chore :: FileUtil 합치는 것으로 수정

--- team-b-batch/src/main/kotlin/com/address/dsmproject/util/FileUtil.kt
+++ team-b-batch/src/main/kotlin/com/address/dsmproject/util/FileUtil.kt
@@ -1,13 +1,25 @@
 package com.address.dsmproject.util
 
+import com.address.dsmproject.dto.UnzipTargetFile
 import org.springframework.stereotype.Component
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.zip.ZipFile
 
 @Component
 class FileUtil {
 
-    fun unzip(zipFile: File, unzipFileTargetDirectory: File) {
+    fun writeFile(unzipTargetFile: UnzipTargetFile, target: ByteArray) {
+        Files.write(Paths.get(unzipTargetFile.zipFilePath), target)
+        val zipFile = File(unzipTargetFile.zipFilePath)
+        val unzipTargetDirectory = File(unzipTargetFile.unzipTargetDirectoryPath)
+        unzipTargetDirectory.mkdir()
+
+        unzip(zipFile, unzipTargetDirectory)
+    }
+
+    private fun unzip(zipFile: File, unzipFileTargetDirectory: File) {
         ZipFile(zipFile).use { zip ->
             for (entry in zip.entries()) {
                 val entryFileOutputStream = File(unzipFileTargetDirectory, entry.name).outputStream()
