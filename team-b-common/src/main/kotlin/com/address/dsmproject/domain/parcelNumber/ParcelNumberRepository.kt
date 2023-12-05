7da4a9972a9e034598d7873e99f0696cb07b007e
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-12-05 16:56:13 +0900

    chore :: (#13) 사용하지 않는 repository 제거

--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/parcelNumber/ParcelNumberRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.address.dsmproject.domain.parcelNumber
-
-import org.springframework.data.repository.CrudRepository
-import java.util.UUID
-
-interface ParcelNumberRepository : CrudRepository<ParcelNumberEntity, UUID> {
-}
--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadAddress/RoadAddressRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.address.dsmproject.domain.roadAddress
-
-import org.springframework.data.repository.CrudRepository
-import java.util.UUID
-
-interface RoadAddressRepository : CrudRepository<RoadAddressEntity, UUID> {
-}
