527bd6ffc27a2e26a246b0cda74f030a9a4a7610
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-12-05 20:09:22 +0900

    chore :: (#13) repository pk 수정

--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/RoadNumberRepository.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/RoadNumberRepository.kt
@@ -1,6 +1,6 @@
 package com.address.dsmproject.domain.roadNumber
 
 import org.springframework.data.repository.CrudRepository
+import java.util.UUID
 
-interface RoadNumberRepository : CrudRepository<RoadNumberEntity, RoadNumberId> {
-}
+interface RoadNumberRepository : CrudRepository<RoadNumberEntity, UUID>
