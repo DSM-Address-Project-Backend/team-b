ae910e7885673af444de7f22e4ef04d334b53737
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-12-07 22:28:00 +0900

    chore: (#13) Persistable 구현

--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/RoadNumberEntity.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/RoadNumberEntity.kt
@@ -7,13 +7,10 @@ import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
-import java.util.UUID
 
 @Entity
 @Table(name = "tbl_road_number")
 class RoadNumberEntity(
-    id: UUID? = null,
-
     @Column(columnDefinition = "VARCHAR(40)", nullable = false)
     val cityProvinceName: String,
 
