9d725295dfca11bf4b11745c755ea5e91f9a42bb
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-12-08 09:29:55 +0900

    chore: (#13) jparepository로 변경

--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/RoadNumberRepository.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/RoadNumberRepository.kt
@@ -1,6 +1,6 @@
 package com.address.dsmproject.domain.roadNumber
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface RoadNumberRepository : CrudRepository<RoadNumberEntity, UUID>
+interface RoadNumberRepository : JpaRepository<RoadNumberEntity, UUID>
