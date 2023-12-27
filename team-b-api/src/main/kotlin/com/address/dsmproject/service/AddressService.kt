319428361668b48564b0b06f2619f0c5b01ee90d
Kangmin <ehfehf0629@gmail.com>
2023-12-27 22:03:22 +0900

    feat :: 자동완성 api 초안

--- team-b-api/src/main/kotlin/com/address/dsmproject/service/AddressService.kt
+++ team-b-api/src/main/kotlin/com/address/dsmproject/service/AddressService.kt
@@ -2,12 +2,17 @@ package com.address.dsmproject.service
 
 import com.address.dsmproject.domain.roadNumber.RoadNumberEntity
 import com.address.dsmproject.domain.roadNumber.repository.AddressRepository
+import com.address.dsmproject.domain.roadNumber.repository.vo.AutoCompletionAddressVO
 import org.springframework.stereotype.Service
 
 @Service
 class AddressService(
     private val addressRepository: AddressRepository
 ) {
+
+    fun autoCompletion(keyword: String): List<AutoCompletionAddressVO> {
+        return addressRepository.autoCompletion(keyword, checkKeywordLanguage(keyword))
+    }
 
     fun getAddress(keyword: String, page: Long): List<RoadNumberEntity> {
         return addressRepository.queryAddress(
--- team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/repository/AddressRepository.kt
+++ team-b-common/src/main/kotlin/com/address/dsmproject/domain/roadNumber/repository/AddressRepository.kt
@@ -2,19 +2,32 @@ package com.address.dsmproject.domain.roadNumber.repository
 
 import com.address.dsmproject.domain.roadNumber.QRoadNumberEntity.roadNumberEntity
 import com.address.dsmproject.domain.roadNumber.RoadNumberEntity
+import com.address.dsmproject.domain.roadNumber.repository.vo.AutoCompletionAddressVO
+import com.address.dsmproject.domain.roadNumber.repository.vo.QAutoCompletionAddressVO
 import com.querydsl.core.types.dsl.BooleanExpression
-import com.querydsl.core.types.dsl.Expressions
-import com.querydsl.core.types.dsl.Expressions.booleanTemplate
 import com.querydsl.core.types.dsl.Expressions.numberTemplate
-import com.querydsl.core.types.dsl.NumberTemplate
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
-
 
 @Repository
 class AddressRepository(
     private val jpaQueryFactory: JPAQueryFactory,
 ) {
+
+    fun autoCompletion(keyword: String, type: String): List<AutoCompletionAddressVO> {
+        return jpaQueryFactory
+            .select(
+                QAutoCompletionAddressVO(
+                    roadNumberEntity.cityProvinceName,
+                    roadNumberEntity.countyDistricts,
+                    roadNumberEntity.eupMyeonDong
+                )
+            )
+            .from(roadNumberEntity)
+            .where(searchFulltext(keyword, type))
+            .limit(LIMIT)
+            .fetch()
+    }
 
     fun queryAddress(
         keyword: String,
@@ -29,9 +42,11 @@ class AddressRepository(
         )
 
         return jpaQueryFactory
-                .selectFrom(roadNumberEntity)
-                .where(numberTemplate.gt(0))
-                .fetch()
+            .selectFrom(roadNumberEntity)
+            .where(numberTemplate.gt(0))
+            .offset(page * LIMIT)
+            .limit(LIMIT)
+            .fetch()
     }
 
     private fun searchFulltext(keyword: String, type: String): BooleanExpression? {
@@ -39,15 +54,23 @@ class AddressRepository(
             return null
         }
         return when (type) {
-            "KOREAN" -> Expressions.numberTemplate(
-                Integer::class.java,
-                "function('match',{0},{1})", roadNumberEntity.korFullText, keyword
+            "KOREAN" -> numberTemplate(
+                Double::class.javaObjectType,
+                "function('match', {0}, {1})",
+                roadNumberEntity.korFullText,
+                keyword
             ).gt(0)
 
-            else -> Expressions.numberTemplate(
-                Integer::class.java,
-                "function('match',{0},{1})", roadNumberEntity.korFullText, keyword
+            else -> numberTemplate(
+                Double::class.javaObjectType,
+                "function('match', {0}, {1})",
+                roadNumberEntity.engFullText,
+                keyword
             ).gt(0)
         }
+    }
+
+    companion object {
+        const val LIMIT = 10L
     }
 }
