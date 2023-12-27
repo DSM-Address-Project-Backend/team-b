021fd980e930bf2d62e5656c85a172ad3d325c05
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-12-27 22:04:29 +0900

    chore: (#13) embeddable noarg, open 제거

--- team-b-common/build.gradle.kts
+++ team-b-common/build.gradle.kts
@@ -26,11 +26,9 @@ tasks.getByName<Jar>("bootJar") {
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
 }
 
 noArg {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
 }
